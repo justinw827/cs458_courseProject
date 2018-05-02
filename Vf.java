3fd3792a12c3fe3049b017c620629d5e412b6c4a
Justin Wang <jwang138@cs.binghamton.edu>
2018-05-02 16:37:55 -0400

    Added client and server files

---
+++ Vf.java
@@ -0,0 +1,182 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.SSLServerSocketFactory;
+import java.security.MessageDigest;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.BadPaddingException;
+import java.security.spec.*;
+import java.nio.file.*;
+import java.security.*;
+import java.io.*;
+
+public class Vf {
+
+	//initialize socket and input stream
+    private Socket          socket   = null;
+    private ServerSocket    server   = null;
+
+    private PrivateKey priv;
+    private PublicKey pub;
+ 
+    // constructor with port
+    public Vf(int port) {
+        // starts server and waits for a connection
+        try {
+            // Create a SSL Server Socket Factory
+            SSLServerSocketFactory sslServerSocketFactory = 
+                (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+
+            // Create a SSL Server Socket with SSL Socket Factory 
+            server = sslServerSocketFactory.createServerSocket(port);
+            System.out.println("SSL Server started");
+ 
+            System.out.println("Waiting for a client ...");
+ 
+            socket = server.accept();
+            System.out.println("Client accepted");
+ 
+            // out.println("string") sends a string over the socket
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            try { 
+                // Buffered reader for socket input
+                BufferedReader br = 
+                    new BufferedReader(
+                            new InputStreamReader(socket.getInputStream()));
+
+                String line = br.readLine();
+
+                // Decrypt line
+
+                
+                String[] tokens = line.split(":");
+                String username = tokens[0]; // Get username
+                String password = tokens[1]; // Get password
+
+                // Open file with name "password.txt"
+                File file = new File("password.txt");
+                FileReader fr;
+                BufferedReader brFile; // Buffered reader for file
+
+                // Password file format
+                // <user ID> <hashed password> <date and time when the password is stored>
+                String idCheck = "";
+                String fileLine = "";
+                String storedPW = "";
+                String date = ""; // Date and time
+                try {
+                    fr = new FileReader(file);
+                    brFile = new BufferedReader(fr);
+                     // Look for matching username in password file
+                    while (idCheck != username && fileLine != null) {
+                        fileLine = brFile.readLine();
+                        String[] tokens2 = fileLine.split(" ");
+                        storedPW = tokens2[1]; // Store password
+                        date = tokens2[2]; // Store date and time
+                        idCheck = tokens2[0];
+                    }
+                } catch (FileNotFoundException e) {
+                    System.err.println("Error opening file.");
+                    e.printStackTrace();
+                }
+
+                // Hash password with MD5
+                String hashtext = hashMD5(password);
+
+                System.out.println(hashtext);
+
+                if (idCheck == username) {
+                    if (hashtext == storedPW) {
+                        out.println("OK");
+                    } else {
+                        out.println("Incorrect");
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.err.println("Error while reading from socket.");
+            }
+
+            System.out.println("Closing connection");
+ 
+            // close connection
+            socket.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.err.println("Error using socket.");
+        }
+    }
+
+    private String hashMD5(String textIn) {
+        // Hash password from client
+        String hashtext = "";
+        try {
+            String plaintext = textIn;
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.reset();
+            m.update(plaintext.getBytes());
+            byte[] digest = m.digest();
+            BigInteger bigInt = new BigInteger(1,digest);
+            hashtext = bigInt.toString(16); // Hashed password as string
+            // Now we need to zero pad it if you actually want the full 32 chars.
+            while (hashtext.length() < 32 ) {
+                hashtext = "0"+hashtext;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Error running MD5.");
+            e.printStackTrace();
+        }
+
+        return hashtext;
+    }
+
+    public void generateKeyPairs() throws NoSuchAlgorithmException, FileNotFoundException, IOException, NoSuchProviderException {
+        // Generate Keys
+        // Key pair code found here: https://docs.oracle.com/javase/tutorial/security/apisign/step2.html
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA", "SUN");
+
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        keyGen.initialize(1024, random);
+
+        KeyPair pair = keyGen.generateKeyPair();
+        priv = pair.getPrivate();
+        pub = pair.getPublic();
+
+        /* save the public key in a file */
+        byte[] key = pub.getEncoded();
+        FileOutputStream keyfos = new FileOutputStream("pubKey1");
+        keyfos.write(key);
+        keyfos.close();
+    }
+
+    public byte[] encrypt(PublicKey publicKey, String message) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");  
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);  
+
+        return cipher.doFinal(message.getBytes());  
+    }
+
+    public byte[] decrypt(PrivateKey key, String ciphertext) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");   
+        cipher.init(Cipher.DECRYPT_MODE, key);  
+        return cipher.doFinal(ciphertext.getBytes());
+    }
+ 
+    public static void main(String[] args) {
+
+    	if (args.length != 1 ) {
+			System.err.println("Error: Incorrect number of arguments. Program accepts 1 argumnet.");
+			System.exit(0);
+		}
+
+		int portNum = Integer.parseInt(args[0]);
+
+        Vf server = new Vf(portNum);
+    }
+}
---
+++ VoterCli.java
@@ -0,0 +1,187 @@
+import java.net.*;
+import java.io.*;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.security.*;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.BadPaddingException;
+import java.security.spec.*;
+import java.nio.file.*;
+
+public class VoterCli {
+    // initialize socket and input output streams
+    private SSLSocket        socket  = null;
+    private BufferedReader   input   = null;
+    private PrintWriter		 out     = null;
+    private BufferedReader   serverOutput = null;
+
+    private PrivateKey priv;
+    private PublicKey pub;
+ 
+    // Constructor to put ip address and port
+    public VoterCli(String address, int port) {
+        // establish a connection
+        try {
+            SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            socket = (SSLSocket) sslsocketfactory.createSocket(address, port);
+            System.out.println("Connected");
+ 
+            // Create buffered reader to get input from console
+            input = new BufferedReader(new InputStreamReader(System.in));
+ 	
+            // Sends output to the socket
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch(UnknownHostException u) {
+            u.printStackTrace();
+			System.err.println("Error connecting to server.");
+        } catch(IOException i) {
+            i.printStackTrace();
+			System.err.println("Error connecting to server.");
+        }
+ 
+ 
+
+	    try {
+
+            generateKeyPairs();
+
+	        // Get name
+			System.out.println("Enter name.");
+			String name = input.readLine();
+
+			// Get vNumber
+			System.out.println("Enter voter registration number.");
+			String vNumber = input.readLine();
+
+            // Digitally sign the name
+            // Digital Signature code found here: https://docs.oracle.com/javase/tutorial/security/apisign/step3.html
+            Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
+            dsa.initSign(priv);
+
+            byte[] nameBuff = name.getBytes();
+            dsa.update(nameBuff, 0, nameBuff.length);
+
+            byte[] realSig = dsa.sign();
+
+            /* save the signature in a file */
+            // Source: https://docs.oracle.com/javase/tutorial/security/apisign/step4.html
+            FileOutputStream sigfos = new FileOutputStream("digitalSignature");
+            sigfos.write(realSig);
+            sigfos.close();
+
+			// Encrypt name, vNumber, and digital signature using server's public key into one message
+            PublicKey serverPubKey = readPublicKey("serverPubKey");
+            byte[] message = encrypt(serverPubKey, name+vNumber+realSig);
+
+            // Send encrypted message
+	        out.println(message.toString());
+
+	        // Get response from server
+	        serverOutput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	        String response = "";
+	        while ((response = serverOutput.readLine()) != null) {
+	        	System.out.println(response);
+	        }    
+
+	        if (response == "OK") {
+	        	System.out.println("the vNumber is correct");
+	        } else {
+	        	System.out.println("the vNumber is incorrect");
+	        }
+
+	    } catch (IOException e) {
+	        e.printStackTrace();
+			System.err.println("Error while reading input.");
+	    } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            System.err.println("Error generating key pairs.");
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+            System.err.println("Error writing digital signature.");
+        } catch (SignatureException e) {
+            e.printStackTrace();
+            System.err.println("Error writing digital signature.");
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+            System.err.println("Error writing digital signature.");
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+            System.err.println("Error encrypting message.");
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+            System.err.println("Error encrypting message.");
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+            System.err.println("Error encrypting message.");
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+            System.err.println("Error reading public key.");
+        }
+         
+        // close the connection
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        } catch(IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public void generateKeyPairs() throws NoSuchAlgorithmException, FileNotFoundException, IOException, NoSuchProviderException {
+        // Generate Keys
+        // Key pair code found here: https://docs.oracle.com/javase/tutorial/security/apisign/step2.html
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA", "SUN");
+
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        keyGen.initialize(1024, random);
+
+        KeyPair pair = keyGen.generateKeyPair();
+        priv = pair.getPrivate();
+        pub = pair.getPublic();
+
+        /* save the public key in a file */
+        byte[] key = pub.getEncoded();
+        FileOutputStream keyfos = new FileOutputStream("pubKey1");
+        keyfos.write(key);
+        keyfos.close();
+    }
+
+    public byte[] encrypt(PublicKey publicKey, String message) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");  
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);  
+
+        return cipher.doFinal(message.getBytes());  
+    }
+
+    public byte[] decrypt(PrivateKey key, String ciphertext) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA1AndMGF1Padding");   
+        cipher.init(Cipher.DECRYPT_MODE, key);  
+        return cipher.doFinal(ciphertext.getBytes());
+    }
+
+    public byte[] readFileBytes(String filename) throws IOException {
+        Path path = Paths.get(filename);
+        return Files.readAllBytes(path);        
+    }
+
+    public PublicKey readPublicKey(String filename) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        X509EncodedKeySpec publicSpec = new X509EncodedKeySpec(readFileBytes(filename));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(publicSpec);       
+    }
+ 
+    public static void main(String[] args) {
+
+        if (args.length != 2 ) {
+            System.err.println("Error: Incorrect number of arguments. Program accepts 2 argumnets.");
+            System.exit(0);
+        }
+
+        String address = args[0];
+        int portNum = Integer.parseInt(args[1]);
+        VoterCli client = new VoterCli(address, portNum);
+    }
+}
